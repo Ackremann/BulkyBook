53e15298b4de14e253bb650cc2c5e096c228d033
Ackremann <59745489+Ackremann@users.noreply.github.com>
2022-06-07 22:59:55 +0200

    Add project files.

---
+++ BulkyBookWeb/Controllers/CatagoryController.cs
@@ -0,0 +1,112 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CatagoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public CatagoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Catagory> objCatagoryList = _db.Catagories;
+            return View(objCatagoryList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Catagory obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display Order Cannot be the same as Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Catagories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Catagory Created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var ctagoryFromDb = _db.Catagories.Find(id);
+            if (ctagoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ctagoryFromDb);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Catagory obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display Order Cannot be the same as Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Catagories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Catagory Updated successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var ctagoryFromDb = _db.Catagories.Find(id);
+            if (ctagoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ctagoryFromDb);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Catagories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+
+            }
+            _db.Catagories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Catagory Deleted successfully";
+
+            return RedirectToAction("Index");
+           
+        }
+    }
+
+}
